d4ef7b83291628655a8ac9afc78b834eb80e415d
mayaradynie <163149897+mayaradynie@users.noreply.github.com>
2024-03-12 11:24:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://ogimg.infoglobo.com.br/in/24097539-f64-d49/FT1086A/85843852_Players-of-Brazils-Flamengo-celebrate-on-the-podium-with-the-trophy-after-winning-the-Copa.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://tntsports.com.br/__export/1694960541500/sites/esporteinterativo/img/2023/09/10/gazeta-press-foto-1924304_1.jpg_1758632412.jpg";
+}
+
